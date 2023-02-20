527954fba8dab67cc1d0a3a40028fdee519dbf9e
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-02-19 20:49:20 -0800

    Add python code

---
+++ code.py
@@ -0,0 +1,11 @@
+import prestodb
+conn=prestodb.dbapi.connect(
+    host='localhost',
+    port=8080,
+    catalog='hive',
+    schema='default',
+)
+cur = conn.cursor()
+cur.execute('show tables')
+rows = cur.fetchall()
+print(rows)
