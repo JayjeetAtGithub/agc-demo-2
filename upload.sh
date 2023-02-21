83a66b935ce47d28bac36f63c0abb8afd62b91f6
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-02-20 18:43:01 -0800

    force create dataset dir

--- upload.sh
+++ upload.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -ex
 
+docker exec -it docker-presto-namenode-1 hadoop fs -mkdir -p /dataset/
 for i in {1..64};
 do
     docker exec -it docker-presto-namenode-1 hadoop fs -put -f /data/Run2012B_SingleMu-1000.${i}.parquet /dataset/
