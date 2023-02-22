8f71b57585ae387e33cf8743e02d2030d4806d9e
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-02-22 10:02:59 -0800

    Restructure

--- upload.sh
+++ upload.sh
@@ -4,6 +4,6 @@ set -ex
 docker exec -it docker-presto-namenode-1 hadoop fs -mkdir -p /dataset/
 for i in {1..64};
 do
-    docker exec -it docker-presto-namenode-1 hadoop fs -put -f /data/Run2012B_SingleMu-1000.${i}.parquet /dataset/
-    docker exec -it docker-presto-namenode-1 hadoop fs -put -f /data/Run2012B_SingleMu-restructured-1000.${i}.parquet /dataset/
+    docker exec -it docker-presto-namenode-1 hadoop fs -put -f /data/Run2012B_SingleMu-1000.${i}.parquet /dataset_nested/
+    docker exec -it docker-presto-namenode-1 hadoop fs -put -f /data/Run2012B_SingleMu-restructured-1000.${i}.parquet /dataset_unnested/
 done
