6408c05e7da83acd6ddced5eb328bc97b2bd17cd
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-02-23 20:45:27 -0800

    Fix scriot

--- presto/create_dataset.sh
+++ presto/create_dataset.sh
@@ -3,7 +3,7 @@ set -ex
 
 for i in {1..64};
 do
-    cp Run2012B_SingleMu-restructured-1000.parquet ../docker-presto/data/Run2012B_SingleMu-restructured-1000.${i}.parquet
+    cp Run2012B_SingleMu-restructured-1000.parquet ../../docker-presto/data/Run2012B_SingleMu-restructured-1000.${i}.parquet
 done
 
 docker exec -it docker-presto-namenode-1 hadoop fs -mkdir -p /dataset/
